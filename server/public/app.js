554928afc152b2a9292e1daab704fefb03809763
Konnor-Young <konnor.young93@gmail.com>
2022-07-13 11:32:45 -0600

    save front end

--- server/public/app.js
+++ server/public/app.js
@@ -1,5 +1,28 @@
+
+
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
-    data: {},
+    data: {
+        message: "HI WORLD",
+        currentPage: "viewInventory",
+        addCardSubPage: "searchCard",
+        addOrderSubPage: "searchCard",
+        // viewInventory: false,
+        // viewOrders: false,
+        // addCard: false,
+        // searchCard: false,
+        // addToPile: false,
+        // pile: false,
+    },
+
+    methods: {
+        goToViewInventory: function () {
+            this
+        },
+    
+        goToAddCard: function () {
+            this.addCard = true;
+        },
+    }
 });
