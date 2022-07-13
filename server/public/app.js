0c5f4db1b91148a8b91341cb7a0671d8a0282fb0
Cody <cody.lehnhausen@gmail.com>
2022-07-13 12:46:06 -0600

    end day 1 of coding

--- server/public/app.js
+++ server/public/app.js
@@ -1,42 +1,45 @@
-const URL = "http://localhost:8080"
+const URL = 'http://localhost:8080';
 
 new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: {
-        message: "HI WORLD",
-        currentPage: "Inventory",
-        addCardSubPage: "searchCard",
-        addOrderSubPage: "searchCard",
-        cardList: ["test", "Test"],
-        orderList: ["order1", "order2"],
-        // viewInventory: false,
-        // viewOrders: false,
-        // addCard: false,
-        // searchCard: false,
-        // addToPile: false,
-        // pile: false,
-    },
-    methods: {
-        getCards: async function () {
-            let response = await fetch(`${URL}/cards`, {
-                method: "GET",
-                credentials: "include"
-            });
+  el: '#app',
+  vuetify: new Vuetify(),
+  data: {
+    message: 'HI WORLD',
+    currentPage: 'Inventory',
+    addCardSubPage: 'searchCard',
+    addOrderSubPage: 'searchCard',
+    cardList: ['test', 'Test'],
+    orderList: ['order1', 'order2'],
+    // viewInventory: false,
+    // viewOrders: false,
+    // addCard: false,
+    // searchCard: false,
+    // addToPile: false,
+    // pile: false,
+  },
+  methods: {
+    getCards: async function () {
+      let response = await fetch(`${URL}/cards`, {
+        method: 'GET',
+        credentials: 'include',
+      });
 
-            let data = await response.json();
-            console.log(data);
-            console.log(this.cardList);
-        },
-        getOrders: async function () {
-            let response = await fetch(`${URL}/orders`, {
-                method: "GET",
-                credentials: "include"
-            });
-
-            let data = await response.json();
-            console.log(data);
-            console.log(this.orderList);
-        },
+      let data = await response.json();
+      console.log(data);
+      console.log(this.cardList);
     },
+    // getOrders: async function () {
+    //   let response = await fetch(`${URL}/orders`, {
+    //     method: 'GET',
+    //     credentials: 'include',
+    //   });
+
+    //   let data = await response.json();
+    //   console.log(data);
+    //   console.log(this.orderList);
+    // },
+    getOrders: function () {
+      console.log('button working');
+    },
+  },
 });
