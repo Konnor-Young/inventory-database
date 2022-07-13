5d757627b233cb5c9c6d95c2a0e26ad975c802ce
Konnor-Young <konnor.young93@gmail.com>
2022-07-13 12:19:33 -0600

    faux POST requests

--- server/server.js
+++ server/server.js
@@ -13,15 +13,45 @@ app.get("/cards", async (req, res)=>{
         console.log('could not find card list', err);
         res.status(500).json({message: 'cards not found', err: err});
     }
-})
-
+    res.status(200).json(cardList);
+});
 app.get("/orders", async (req, res)=>{
     let orderList;
-    try {} catch (err) {
+    try {
+        orderList = await orderList.find({});
+    } catch (err) {
         console.log(`could not find orders`, err);
         res.status(500).json({message: `orders not found`, err: err});
     }
-})
+    res.status(200).json(orderList);
+});
+
+app.post("/cards", async (req, res)=>{
+    try {
+        let card = Card.create({
+            name: req.body.name,
+            condition: req.body.condition,
+            price: req.body.price
+        });
+        res.status(201).json(card);
+    } catch (err) {
+        console.log(`could not create`, err);
+        res.status(500).json({message: `could not create`, err: err});
+    }
+});
+app.post("/orders", async (req, res)=>{
+    try {
+        let order = Order.create({
+            number: req.body.number,
+            price: req.body.price,
+            direct: req.body.direct
+        });
+        res.status(201).json(order);
+    } catch (err) {
+        console.log(`could not create`, err);
+        res.status(500).json({message: `could not create`, err: err});
+    }
+});
 
 module.exports = {
     server: app
