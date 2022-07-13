b3e4e3991f62440fcab30ff06f83d5eb2e9c1e8e
Konnor-Young <konnor.young93@gmail.com>
2022-07-13 12:23:43 -0600

    require in model

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const app = express();
 const cors = require(`cors`);
+const { Card, Order } = require(`../persist/model`);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
