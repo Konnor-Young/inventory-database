2205ec3436f1d17a209051622e2951bdcf1a4348
Cody <cody.lehnhausen@gmail.com>
2022-07-14 10:24:35 -0600

    post functions

--- server/public/app.js
+++ server/public/app.js
@@ -1,41 +1,68 @@
-const URL = "http://localhost:8080"
+const URL = 'http://localhost:8080';
 // vuetify: new Vuetify(),
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: "HI WORLD",
-        currentPage: "Inventory",
-        addCardSubPage: "searchCard",
-        addOrderSubPage: "searchCard",
-        cardList: [],
-        orderList: [],
+  el: '#app',
+  data: {
+    message: 'HI WORLD',
+    currentPage: 'Inventory',
+    addCardSubPage: 'searchCard',
+    addOrderSubPage: 'searchCard',
+    cardList: [],
+    orderList: [],
+    orderNumber: 0,
+    orderPrice: '',
+    directStatus: false,
+  },
+  methods: {
+    getCards: async function () {
+      let response = await fetch(`${URL}/cards`, {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      let data = await response.json();
+      this.cardList = data;
+      console.log(data);
+      console.log(this.cardList);
     },
-    methods: {
-        getCards: async function () {
-            let response = await fetch(`${URL}/cards`, {
-                method: "GET",
-                credentials: "include"
-            });
+    getOrders: async function () {
+      let response = await fetch(`${URL}/orders`, {
+        method: 'GET',
+        credentials: 'include',
+      });
 
-            let data = await response.json();
-            this.cardList = data;
-            console.log(data);
-            console.log(this.cardList);
-        },
-        getOrders: async function () {
-            let response = await fetch(`${URL}/orders`, {
-                method: "GET",
-                credentials: "include"
-            });
+      let data = await response.json();
+      this.orderList = data;
+      console.log(data);
+      console.log(this.orderList);
+    },
+    newOrder: function () {
+      let newOrder = {
+        number: this.orderNumber,
+        price: this.orderPrice,
+        direct: this.directStatus,
+      };
 
-            let data = await response.json();
-            this.orderList = data;
-            console.log(data);
-            console.log(this.orderList);
+      this.postOrder(order);
+    },
+
+    postOrder: async function (order) {
+      let response = await fetch(`${URL}/orders`, {
+        method: 'POST',
+        body: JSON.stringify(order),
+        headers: {
+          'content-type': 'application/json',
         },
+        credentials: 'include',
+      });
+      let data = await response.json();
+      if (response.status == 200) {
+        this.getOrders();
+      }
     },
     created: function () {
-        this.getCards();
-        this.getOrders();
+      this.getCards();
+      this.getOrders();
     },
+  },
 });
