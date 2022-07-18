5a8fa1c477cc429065afef9ca685337bd5cbad7d
Konnor-Young <konnor.young93@gmail.com>
2022-07-17 22:03:27 -0600

    updated schemas

--- persist/model.js
+++ persist/model.js
@@ -2,21 +2,33 @@ const mongoose = require("mongoose");
 
 const cardSchema = mongoose.Schema({
       name: { type: String },
-      condition: { type: String, enum: ['nm', 'lp','hp','mp'] },
-      price: { type: mongoose.Types.Decimal128},
-      game: { type:String, enum: ['MTG', 'Pokemon']}     
+      location: { type: String },
+      foil: { type:String, enum: ['None', 'Foil']},     
+});
+
+const skuSchema = mongoose.Schema({
+    tcgID: { type: String },
+    image: { type: String },
+    locations: { type: [String] },
+    quantity: { type: Map, of: Number },
+    name: { type: String },
+    cards: {type: [cardSchema] },
+    price: { type: String }
 });
 
 const orderSchema = mongoose.Schema({
-    number: { type: Number },
-    price: { type: mongoose.Types.Decimal128},
-    direct: { type: Boolean}
+    number: { type: String },
+    direct: { type: Boolean },
+    card: { type: Map, of: Number },
+    status: { type: String, enum: ['standing', 'pulling', 'shipped'] },
 });
 
 const Card = mongoose.model("Card", cardSchema);
 const Order = mongoose.model("Order", orderSchema);
+const Sku = mongoose.model("Sku", skuSchema);
 
 module.exports = {
     Card,
     Order,
+    Sku,
 }
