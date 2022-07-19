14050fa056d216ce9f0f998dbcfd59b5b392c0df
[ryan larson] <[rylarso@gmail.com]>
2022-07-19 11:55:44 -0600

    delete card working

--- server/server.js
+++ server/server.js
@@ -6,23 +6,23 @@ const { Card, Order, Sku, } = require(`../persist/model`);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`));
 
-app.get("/cards", async (req, res)=>{
+app.get("/cards", async (req, res) => {
     let uniqueCards;
     try {
         uniqueCards = await Sku.find({});
     } catch (err) {
         console.log('could not find card list', err);
-        res.status(500).json({message: 'cards not found', err: err});
+        res.status(500).json({ message: 'cards not found', err: err });
     }
     res.status(200).json(uniqueCards);
 });
-app.get("/orders", async (req, res)=>{
+app.get("/orders", async (req, res) => {
     let orderList;
     try {
         orderList = await Order.find({});
     } catch (err) {
         console.log(`could not find orders`, err);
-        res.status(500).json({message: `orders not found`, err: err});
+        res.status(500).json({ message: `orders not found`, err: err });
     }
     res.status(200).json(orderList);
 });
@@ -40,7 +40,7 @@ app.get("/orders", async (req, res)=>{
 //         res.status(500).json({message: `could not create`, err: err});
 //     }
 // });
-app.post("/cards", async (req, res)=>{
+app.post("/cards", async (req, res) => {
     let sku;
     try {
         sku = await Sku.findOne({
@@ -52,13 +52,17 @@ app.post("/cards", async (req, res)=>{
                 name: req.body.name,
                 set: req.body.set,
                 locations: ['here'],
-                image_uris: { small: req.body.image_uris.small, normal: req.body.image_uris.normal,
-                    large: req.body.image_uris.large, png: req.body.image_uris.png, 
-                    border_crop: req.body.image_uris.border_crop },
+                image_uris: {
+                    small: req.body.image_uris.small, normal: req.body.image_uris.normal,
+                    large: req.body.image_uris.large, png: req.body.image_uris.png,
+                    border_crop: req.body.image_uris.border_crop
+                },
                 price: { usd: req.body.prices.usd, usd_foil: req.body.prices.usd_foil },
                 quantity: { available: 1, reserved: 0, physical: 1 },
-                art: { borderless: false, textless: false, etched: false, full_art: false,
-                    promo: false, oversized: false }
+                art: {
+                    borderless: false, textless: false, etched: false, full_art: false,
+                    promo: false, oversized: false
+                }
             });
         }
         sku = await Sku.findByIdAndUpdate(
@@ -75,15 +79,15 @@ app.post("/cards", async (req, res)=>{
                     },
                 },
             },
-            {new: true,}
+            { new: true, }
         );
     } catch (err) {
         console.log(`could not create`, err);
-        res.status(500).json({message: `could not create`, err: err});
+        res.status(500).json({ message: `could not create`, err: err });
     }
     res.status(200).json(sku);
 });
-app.post("/orders", async (req, res)=>{
+app.post("/orders", async (req, res) => {
     try {
         let order = Order.create({
             number: req.body.number,
@@ -93,7 +97,7 @@ app.post("/orders", async (req, res)=>{
         res.status(201).json(order);
     } catch (err) {
         console.log(`could not create`, err);
-        res.status(500).json({message: `could not create`, err: err});
+        res.status(500).json({ message: `could not create`, err: err });
     }
 });
 // app.patch("/cards/:id", async (req, res)=>{
@@ -113,13 +117,13 @@ app.post("/orders", async (req, res)=>{
 //         res.status(500).json({message: `could not create`, err: err});
 //     }
 // });
-app.patch("/orders/:id", async (req, res)=>{
+app.patch("/orders/:id", async (req, res) => {
     let order;
     try {
-        order = await Order.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!order) {
             res.status(404).json({
-              message: "Order Not Found!",
+                message: "Order Not Found!",
             });
             return;
         } else {
@@ -127,22 +131,49 @@ app.patch("/orders/:id", async (req, res)=>{
         }
     } catch (err) {
         console.log(`could not find`, err);
-        res.status(500).json({message: `could not create`, err: err});
+        res.status(500).json({ message: `could not create`, err: err });
     }
 });
-app.delete(`/cards/:id`, async (req, res)=>{
-    let card;
-    try{
-        card = await Sku.findByIdAndDelete(req.params.id);
-        if(!card){
-            res.status(404).json({message: `card not found`});
-            return;
-        }
-        res.status(200).json(card);
-    }catch(err){
-        console.log(`could not delete`, err);
-        res.status(500).json({message: `could not delete`, err: err});
-    }
+app.delete(`/skus/:sku_id/cards/:card_id`, async (req, res) => {
+    // let card;
+    let sku;
+    // try {
+    //     sku = await Sku.findOne({
+    //         _id: req.params.sku_id,
+    //         // "card_id": req.params.id
+    //     });
+    //     // console.log(sku);
+    //     // card = await Sku.findByIdAndDelete(sku.cards.card_id);
+
+    //     if (!sku) {
+    //         res.status(404).json({ message: `sku not found` });
+    //         return;
+    //     }
+    //     // res.status(200).json(card);
+    // } catch (err) {
+    //     console.log(`could not delete`, err);
+    //     res.status(500).json({ message: `could not delete`, err: err });
+    //     return;
+    // }
+
+    sku = await Sku.findByIdAndUpdate(req.params.sku_id, {
+        $pull: {
+            cards: {
+                _id: req.params.card_id
+            },
+        },
+    });
+
+    // if (!sku.cards) {
+    //     sku = await Sku.findByIdAndDelete(req.params.id);
+    // }
+    // } catch (err) {
+    //     res.status(500).json({
+    //         message: "error deleting card",
+    //         error: err
+    //     });
+    // }
+    res.status(200).json(sku);
 });
 
 module.exports = {
